1b9812f6b4cdcd47e67effc307f5265df9bafb32
Jimmy Stammers <jimmy.stammers@gmail.com>
2017-11-16 17:41:13 +0000

    Commit before merge with remote)

---
+++ Utils/OptionsType.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UtilsNS;
+
+namespace OptionsTypeNS
+{
+    public class GeneralOptions
+    {
+        public enum SaveModes { save, ask, nosave }
+
+        public SaveModes saveModes;
+
+        public string SignalCursorPrec { get; set; }
+        public string SignalTablePrec { get; set; }
+        public string SaveFilePrec { get; set; }
+        public bool intN2 { get; set; }
+
+        public void Save()
+        {
+            string fileJson = JsonConvert.SerializeObject(this);
+            File.WriteAllText(Utils.configPath + "genOptions.cfg", fileJson);
+        }
+    }
+
+    public class Modes
+    {
+        // Top
+        public int StackDepth { get; set; }
+
+
+        // Middle
+        public bool ManualYAxis { get; set; }
+        public bool Background { get; set; }
+        public bool DarkCurrent { get; set; }
+        public bool N1 { get; set; }
+        public bool N2 { get; set; }
+        public bool RN1 { get; set; }
+        public bool RN2 { get; set; }
+        public bool Ntot { get; set; }
+
+        // Bottom
+
+        public void Save()
+        {
+            string fileJson = JsonConvert.SerializeObject(this);
+            File.WriteAllText(Utils.configPath + "Defaults.cfg", fileJson);
+        }
+    }
+}
