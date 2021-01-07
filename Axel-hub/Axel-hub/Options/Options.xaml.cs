09f24c5ccf2cf24b159aa513abc413c3da021e04
Teodor Krastev <info@sicyon.com>
2021-01-07 16:30:58 +0000

    Update Options.xaml.cs

--- Axel-hub/Axel-hub/Options/Options.xaml.cs
+++ Axel-hub/Axel-hub/Options/Options.xaml.cs
@@ -20,7 +20,7 @@ namespace OptionsNS
 {
     /// <summary>
     /// Interaction logic, load & save for GeneralOptions genOptions
-    /// some changes 4
+    /// some changes 5
     /// </summary>
     public partial class OptionsWindow : Window
     {
